aea0f80f67e8fab754b51e74f10160e04341e86a
joechesney <33613119+joechesney@users.noreply.github.com>
2018-01-31 16:47:56 -0600

    got it to redirect to addnewnote when a user is registered, then autologged in

--- app/controllers/RegisterCtrl.js
+++ app/controllers/RegisterCtrl.js
@@ -1,21 +1,31 @@
 "use strict";
 
 angular.module("UserNotesApp")
-.controller("RegisterCtrl", function($scope, authFactory, $location){
+.controller("RegisterCtrl", function($scope, authFactory, $window, $location){
   $scope.title = "Please Register";
 
-
-  $scope.register = () =>{
+  $scope.account = {};
+  $scope.register = (account) =>{
     authFactory.registerUser($scope.account)
     .then( (user) =>{
+      $window.location.url("/noteList");
       console.log('newUser',user);
-      authFactory.getUser();
-      $location.url("/login");
-      authFactory.loginUser($scope.account);
+      // authFactory.getUser();
+      authFactory.loginUser(user)
+      .then( someData =>{
+        // $location.url("/noteList");
+        console.log('should have redicrected by now');
+
+      });
     })
     .catch( error=>{
       console.log('errrorq',error);
     });
   };
   
+  // $scope.registerUser = function(registerNewUser) {
+  //   AuthFactory.registerWithEmail(registerNewUser).then(function(didRegister) {
+  //     console.log(didRegister);
+  //   });
+  // };
 });
